eb8c64d1378fcae4cb55d641affdd451a7d77bc7
jordi3d <jordi@cine3d.com>
2022-02-16 14:26:19 +0100

    posant ordre

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> 16af99c (posant ordre)
 import "./App.css";
 import Cv from "./Cv";
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
       <Cv />
->>>>>>> 16af99c (posant ordre)
     </div>
   );
 }
