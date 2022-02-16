12d1f843ecc9bc4f45ceb95b9674e08b1fbd1b2b
jordi3d <jordi@cine3d.com>
2022-02-16 14:12:54 +0100

    posant ordre

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+<<<<<<< HEAD
 import "./App.css";
 import Cv from "./Cv";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> e627a48 (Initialize project using Create React App)
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Cv />
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+>>>>>>> e627a48 (Initialize project using Create React App)
     </div>
   );
 }
