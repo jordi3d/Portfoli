d926049185e6cfa7cb9c75f774166bb9951acccc
jordi3d <jordi@cine3d.com>
2022-03-15 18:32:48 +0100

    separats els títols del codi i externalitzat a fitxers separats

---
+++ src/altres.js
@@ -0,0 +1,93 @@
+export const nom = [
+  {
+    CAT: "Jordi Alonso",
+    ES: "Jorge Alonso",
+    EN: "George Alonso",
+    FR: "George Alonso",
+    RU: "Хорди Алонсо",
+    DE: "George Alonso",
+    IT: "Giorgio Alonso",
+    PT: "Jorge Alonso",
+    NL: "Jordi Alonso",
+    AR: "جوردي ألونسو",
+    ZH: "安桥笛",
+  },
+];
+
+export const congressos = [
+  {
+    CAT: "Congressos",
+    ES: "Congresos",
+    EN: "Congresses",
+    FR: "Congrès",
+    RU: "Конгрессы",
+    DE: "Kongresse",
+    IT: "Congressi",
+    PT: "Congressos",
+    NL: "Congressen",
+    AR: "رجل اعمال",
+    ZH: "国会",
+  },
+];
+
+export const altres = [
+  {
+    CAT: "Altres",
+    ES: "Otros",
+    EN: "Others",
+    FR: "Autres",
+    RU: "Другие",
+    DE: "Andere",
+    IT: "Ottros",
+    PT: "Outras",
+    NL: "Andere",
+    AR: "تعليم",
+    ZH: "其他",
+  },
+];
+export const afiliacions = [
+  {
+    CAT: "Afiliacions",
+    ES: "Afiliaciones",
+    EN: "Affiliations",
+    FR: "Affiliations",
+    RU: "Принадлежности",
+    DE: "Angelegenheiten",
+    IT: "Affiliazioni",
+    PT: "Afiliações",
+    NL: "Affiliaties",
+    AR: "التدريب المنظم",
+    ZH: "隶属关系",
+  },
+];
+export const aficions = [
+  {
+    CAT: "Aficions",
+    ES: "Aficiones",
+    EN: "Hobbies",
+    FR: "Passe-temps",
+    RU: "Хобби",
+    DE: "Hobbys",
+    IT: "Africations",
+    PT: "Hobbies",
+    NL: "Hobby&apos;s",
+    AR: "الدورات",
+    ZH: "爱好",
+  },
+];
+
+export const copyright = [
+  {
+    CAT: "Tots els drets reservats",
+    ES: "Reservados todos los derechos",
+    EN: "All rights reserved",
+    FR: "Tous droits réservés",
+    RU: "Всё права защищены",
+    DE: "Alle Rechte vorbehalten",
+    IT: "Tutti i diritti riservati",
+    PT: "Todos os direitos reservados",
+    NL: "Alle rechten voorbehouden",
+    AR: "صينى",
+    ZH: "版权所有",
+  },
+];
---
+++ src/formacio.js
@@ -0,0 +1,45 @@
+export const formacio_reglada = [
+  {
+    CAT: "Formació Reglada",
+    ES: "Formación Reglada",
+    EN: "Regulated Training",
+    FR: "Formation Réglementée",
+    RU: "Регулируемая обучение",
+    DE: "Reguliertes Training",
+    IT: "Formazione Regolata",
+    PT: "Formação Regulada",
+    NL: "Gereguleerde training",
+    AR: "آحرون",
+    ZH: "监管培训",
+  },
+];
+export const educacio = [
+  {
+    CAT: "Educació",
+    ES: "Educación",
+    EN: "Education",
+    FR: "Éducation",
+    RU: "Образование",
+    DE: "Bildung",
+    IT: "Formazione scolastica",
+    PT: "Educação",
+    NL: "Opleiding",
+    AR: "الخبرة العملية",
+    ZH: "教育",
+  },
+];
+export const cursos = [
+  {
+    CAT: "Cursos",
+    ES: "Cursos",
+    EN: "Courses",
+    FR: "Cours",
+    RU: "Курсы",
+    DE: "Kurse",
+    IT: "Corsi",
+    PT: "Cursos",
+    NL: "Cursussen",
+    AR: "مقابلات",
+    ZH: "培训班",
+  },
+];
