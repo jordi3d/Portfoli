89a7a7d7fa36e0785901881e25776588f161195d
jordi3d <jordi@cine3d.com>
2022-03-19 20:42:27 +0100

    Updates

---
+++ precache-manifest.e72d5744e6236de30771353d3b558037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d314b9be83d9be1e974c",
+    "url": "/Portfoli/static/css/main.ea7f18bb.chunk.css"
+  },
+  {
+    "revision": "d314b9be83d9be1e974c",
+    "url": "/Portfoli/static/js/main.d314b9be.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/Portfoli/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "bb5f21b3670b3090b3c7",
+    "url": "/Portfoli/static/js/2.bb5f21b3.chunk.js"
+  },
+  {
+    "revision": "3f9ca5efffb67e9f33d8",
+    "url": "/Portfoli/static/js/runtime~main.3f9ca5ef.js"
+  },
+  {
+    "revision": "4625baece191dbe9d361a7d1af9d8682",
+    "url": "/Portfoli/index.html"
+  }
+];
